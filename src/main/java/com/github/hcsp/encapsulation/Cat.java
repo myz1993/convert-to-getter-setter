4ad1f36dd94e2e0d8e47f0c5a0f0d40fdb36df15
Bo Zhang <bo@gradle.com>
2019-06-22 19:10:16 +0800

    Sync repositories

---
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -0,0 +1,10 @@
+package com.github.hcsp.encapsulation;
+
+public class Cat {
+    /** 猫咪的名字 */
+    public String name;
+    /** 猫咪的年龄 */
+    public int age;
+    /** 猫咪是否萌，true为萌，false为不萌 */
+    public boolean cute;
+}
---
+++ src/test/java/com/github/hcsp/encapsulation2/CatTest.java
@@ -0,0 +1,32 @@
+package com.github.hcsp.encapsulation2;
+
+import java.lang.reflect.Field;
+import java.util.Random;
+import java.util.UUID;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CatTest {
+    @Test
+    public void hasGettersAndSetters() {
+        String name = UUID.randomUUID().toString();
+        int age = new Random().nextInt(10);
+        com.github.hcsp.encapsulation.Cat cat = new com.github.hcsp.encapsulation.Cat();
+
+        cat.setName(name);
+        cat.setAge(age);
+        cat.setCute(true);
+
+        Assertions.assertEquals(name, cat.getName());
+        Assertions.assertEquals(age, cat.getAge());
+        Assertions.assertTrue(cat.getCute());
+    }
+
+    @Test
+    public void allFieldsArePrivate() {
+        Assertions.assertTrue(
+                Stream.of(com.github.hcsp.encapsulation.Cat.class.getDeclaredFields())
+                        .map(Field::getModifiers)
+                        .allMatch(Modifier::isPrivate));
+    }
+}
