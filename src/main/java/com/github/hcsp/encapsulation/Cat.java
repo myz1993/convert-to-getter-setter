8ebf44528832bb72cd98da8c85f8a3a170447500
余韵之 <36914785+Lambert1129@users.noreply.github.com>
2019-06-24 11:48:54 +0800

    x (#4)
    x (#4)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -2,9 +2,44 @@ package com.github.hcsp.encapsulation;
 
 public class Cat {
     /** 猫咪的名字 */
-    public String name;
+    private String name;
     /** 猫咪的年龄 */
-    public int age;
+    private int age;
     /** 猫咪是否萌，true为萌，false为不萌 */
-    public boolean cute;
+    private boolean cute;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isCute() {
+        return cute;
+    }
+
+    public void setCute(boolean cute) {
+        this.cute = cute;
+    }
+
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.setAge(12);
+        cat.setCute(true);
+        cat.setName("cat");
+
+        System.out.println(cat.getAge());
+        System.out.println(cat.isCute());
+        System.out.println(cat.getName());
+    }
 }
