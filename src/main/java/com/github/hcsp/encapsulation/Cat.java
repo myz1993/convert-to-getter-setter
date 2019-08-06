f21a8e70939ec0349134a8e08c9b8fb5aa777350
fei tang <497045233@qq.com>
2019-08-06 16:49:35 +0800

    将成员改写为getter/setter (#38)
    将成员改写为getter/setter (#38)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -1,10 +1,40 @@
 package com.github.hcsp.encapsulation;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
 public class Cat {
     /** 猫咪的名字 */
-    public String name;
+    private  String name;
     /** 猫咪的年龄 */
-    public int age;
+    private  int age;
     /** 猫咪是否萌，true为萌，false为不萌 */
-    public boolean cute;
+    private  boolean cute;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setCute(Boolean cute) {
+        this.cute = cute;
+    }
+
+    public Boolean isCute(){
+        return this.cute;
+    }
+
+
+
+
 }
