6cec13f15b6d91e0b275838206ae8825bfed1711
Mingru Li <limingru1991@gmail.com>
2019-08-11 22:11:05 +0800

    getter和setter使用 (#40)
    getter和setter使用 (#40)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -2,9 +2,29 @@ package com.github.hcsp.encapsulation;
 
 public class Cat {
     /** 猫咪的名字 */
-    public String name;
+    private String name;
     /** 猫咪的年龄 */
-    public int age;
+    private int age;
     /** 猫咪是否萌，true为萌，false为不萌 */
-    public boolean cute;
+    private boolean cute;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public boolean isCute() {
+        return cute;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setCute(boolean cute) {
+        this.cute = cute;
+    }
 }
