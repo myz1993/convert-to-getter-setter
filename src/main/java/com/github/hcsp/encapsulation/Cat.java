ec65ebeca04be3438f05e8750b5ffc95259d39a2
ShanQing Huang <58178504+7952126@users.noreply.github.com>
2020-03-28 16:26:09 +0800

    setter and getter (#167)
    setter and getter (#167)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -1,10 +1,40 @@
 package com.github.hcsp.encapsulation;
 
 public class Cat {
+    private String name;
+    /**
+     * 猫咪的年龄
+     */
+    private int age;
+    /**
+     * 猫咪是否萌，true为萌，false为不萌
+     */
+    private boolean cute;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isCute() {
+        return cute;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setCute(boolean cute) {
+        this.cute = cute;
+    }
+
     /** 猫咪的名字 */
-    public String name;
-    /** 猫咪的年龄 */
-    public int age;
-    /** 猫咪是否萌，true为萌，false为不萌 */
-    public boolean cute;
+
 }
