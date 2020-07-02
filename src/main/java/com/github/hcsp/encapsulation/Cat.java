e7ae6999346302b3097d748da51b8acb96c34e73
2014 <suhanma@gmail.com>
2020-07-02 10:08:16 -0700

    getter (#198)
    getter (#198)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -1,10 +1,37 @@
 package com.github.hcsp.encapsulation;
 
 public class Cat {
+
+    private String name;
+
+    private int age;
+
+    private boolean cute;
+
     /** 猫咪的名字 */
-    public String name;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     /** 猫咪的年龄 */
-    public int age;
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     /** 猫咪是否萌，true为萌，false为不萌 */
-    public boolean cute;
+    public boolean isCute() {
+        return cute;
+    }
+
+    public void setCute(boolean cute) {
+        this.cute = cute;
+    }
 }
