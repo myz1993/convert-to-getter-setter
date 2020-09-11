d7105e8f1bf701c0b3cc4212fc6779928b23ce55
Faiz <53805895+faiz-lab@users.noreply.github.com>
2020-09-11 21:32:39 +0800

    将成员改写为getter/setter (#207)
    将成员改写为getter/setter (#207)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -2,9 +2,33 @@ package com.github.hcsp.encapsulation;
 
 public class Cat {
     /** 猫咪的名字 */
-    public String name;
+    private String name;
     /** 猫咪的年龄 */
-    public int age;
+    private int age;
     /** 猫咪是否萌，true为萌，false为不萌 */
-    public boolean cute;
+    private boolean cute;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isCute() {
+        return cute;
+    }
+
+    public void setCute(boolean cute) {
+        this.cute = cute;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
