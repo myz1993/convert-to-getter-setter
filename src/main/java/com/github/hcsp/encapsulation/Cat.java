a437dac53b8d1dba6bf0c82b92564c857f7d327b
skysZhang <37018639+skysZhang@users.noreply.github.com>
2021-07-07 14:14:37 +0800

    convert to getter and setter (#230)
    convert to getter and setter (#230)

--- src/main/java/com/github/hcsp/encapsulation/Cat.java
+++ src/main/java/com/github/hcsp/encapsulation/Cat.java
@@ -1,10 +1,43 @@
 package com.github.hcsp.encapsulation;
 
 public class Cat {
-    /** 猫咪的名字 */
-    public String name;
-    /** 猫咪的年龄 */
-    public int age;
-    /** 猫咪是否萌，true为萌，false为不萌 */
-    public boolean cute;
+    /**
+     * 猫咪的名字
+     */
+    private String name;
+    /**
+     * 猫咪的年龄
+     */
+    private int age;
+    /**
+     * 猫咪是否萌，true为萌，false为不萌
+     */
+    private boolean cute;
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isCute() {
+        return cute;
+    }
+
+    public void setCute(boolean cute) {
+        this.cute = cute;
+    }
+
+
 }
